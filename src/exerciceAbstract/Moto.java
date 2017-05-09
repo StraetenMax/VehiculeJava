e0aed502c8c2ebd069b1c71ef932154b1f3b82e9
StraetenMax <maxstraeten@yahoo.com>
2017-05-09 17:22:55 +0200

    classe Moto

--- src/exerciceAbstract/Moto.java
+++ src/exerciceAbstract/Moto.java
@@ -2,6 +2,31 @@ package exerciceAbstract;
 
 public class Moto extends VehiculeTerrien {
 		//propriétés
+	private String marque;
 	
 		//constructeurs
+	public Moto(){
+		super();
+		marque = "";
+	}
+	public Moto(String cCouleur,int cNbOccupants,String cMarque){
+		super(cCouleur, cNbOccupants);
+		marque = cMarque;
+	}
+	
+		//accesseurs mutateurs
+	public String getMarque(){
+		return marque;
+	}
+	
+	public void setMarque(String yMarque){
+		marque = yMarque;
+	}
+	//méthodes
+	public void avancer(){
+		System.out.println("J'avance Moto");
+	}
+	public void seGarer(){
+		System.out.println("Je me gare Moto");
+	}
 }
