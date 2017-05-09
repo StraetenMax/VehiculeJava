e96a4dc88ab0a63e34a31b5f6255c9f6d38aac93
StraetenMax <maxstraeten@yahoo.com>
2017-05-09 16:44:02 +0200

    creation Classe Hélicoptère

--- src/exerciceAbstract/Helicoptere.java
+++ src/exerciceAbstract/Helicoptere.java
@@ -1,5 +1,26 @@
 package exerciceAbstract;
 
-public class Helicoptere {
-
+public class Helicoptere extends VehiculeAerien {
+	//propriété
+	
+	//constructeur
+	public Helicoptere(){
+		super();
+	}
+	
+	public Helicoptere(String cCouleur, int cNbOccupants, int cHeure){
+		super(cCouleur, cNbOccupants, cHeure);
+	}
+	
+	//méthodes
+	public void avancer(){
+		System.out.println("J'avance Hélicoptère");
+	}
+	
+	public void seGarer(){
+		System.out.println("Je me gare Hélicoptère");
+	}
+	
+	
+	
 }
