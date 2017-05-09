f8aa96f4f9949d1a2bb505aaea3e7b12d6efdcbd
StraetenMax <maxstraeten@yahoo.com>
2017-05-09 17:09:28 +0200

    classe Auto

--- src/exerciceAbstract/Auto.java
+++ src/exerciceAbstract/Auto.java
@@ -5,5 +5,31 @@ public class Auto extends VehiculeTerrien{
 	private int nbPortes;
 	
 		//constructeur
+	public Auto(){
+		super();
+		nbPortes = 0;
+	}
+	
+	public Auto(String cCouleur,int cNbOccupants,int cNbPortes){
+		super(cCouleur, cNbOccupants);
+		nbPortes = cNbPortes;
+	}
+	
+		//accesseurs mutateurs
+		public int getNbPortes(){
+			return nbPortes;
+		}
+		public void setNbPortes(int yNbPortes){
+			nbPortes = yNbPortes;
+		}
+		
+		//méthodes
+		public void avancer(){
+			System.out.println("J'avance Auto");
+		}
+		
+		public void seGarer(){
+			System.out.println("Je me gare Auto");
+		}
 	
 }
