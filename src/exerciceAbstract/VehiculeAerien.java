92bf546f5631c70e7ee59805085c48e48d7bdd10
StraetenMax <maxstraeten@yahoo.com>
2017-05-09 15:31:44 +0200

    création classe VehiculeAerien

--- src/exerciceAbstract/VehiculeAerien.java
+++ src/exerciceAbstract/VehiculeAerien.java
@@ -1,5 +1,41 @@
 package exerciceAbstract;
 
-public class VehiculeAerien {
-
+public class VehiculeAerien extends Vehicule {
+	//propriétés
+	private int heure;
+	private boolean longCourrier;
+	
+	//constructeurs
+	public VehiculeAerien(){
+		super();
+		heure = 0;
+		this.setLongCourrier();
+	}
+	public VehiculeAerien(String cCouleur, int cNbOccupants, int cHeure){
+		super(cCouleur, cNbOccupants);
+		heure = cHeure;
+		this.setLongCourrier();
+	}
+	//accesseurs et mutateurs
+	public int getHeure(){
+		return heure;
+	}
+	
+	public boolean getLongCourrier(){
+		return longCourrier;
+	}
+	
+	public void setHeure(int yHeure){
+		heure = yHeure;
+		this.setLongCourrier();
+	}
+	
+	public void setLongCourrier(){
+		if(heure >5){
+			longCourrier = true;
+		}else{
+			longCourrier = false;
+		}
+	}
+	//méthodes
 }
